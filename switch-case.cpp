35da5f94b503a3b8d03d3e5fd478d6d364447f9d
AgastyaDhand1914 <agastyadhand@gmail.com>
2024-08-29 19:44:58 +0530

    Switch-Case Ques

---
+++ switch-case.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+
+    int n;//, a, b;
+//    char op;
+    cin>>n;
+//    cin>>a>>b>>op;
+//Simple Calculator
+    /*switch (op) {
+        case '+': 
+            cout<<a+b<<endl;
+            break;
+
+        case '-': 
+            cout<<a-b<<endl;
+            break;
+        
+        case '*': 
+            cout<<a*b<<endl;
+            break;
+        
+        case '/': 
+            cout<<a/b<<endl;
+            break;
+        
+        case '%': 
+            cout<<a%b<<endl;
+            break;
+
+        default: cout<<"Enter valid values";
+    }*/
+
+//n amount of money to num of 100, 50, 20 & 1 rs notes
+
+/*switch (1) {
+
+    case 1: 
+        cout<<"Rs100 notes: "<<n/100<<endl;
+        n%=100;
+    case 2: 
+        cout<<"Rs50 notes: "<<n/50<<endl;
+        n%=50;
+    case 3: 
+        cout<<"Rs20 notes: "<<n/20<<endl;
+        n%=20;
+    case 4: 
+        cout<<"Rs1 notes: "<<n<<endl;
+
+}*/
+
+//
+
+    return 0;
+ }
