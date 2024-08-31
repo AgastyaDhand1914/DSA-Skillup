b784e0fc6a43301b1f236ca8b8c758e0116008e9
AgastyaDhand1914 <agastyadhand@gmail.com>
2024-09-01 00:06:45 +0530

    Searching sub array in another array

--- DSA_3.cpp
+++ DSA_3.cpp
@@ -1,20 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int sub_Arr(int arr1[], int arr2[]) {
         int count = 0;
         for (int i = 0; i<3; ++i) {
             for (int j = 0; j<10; ++j) {
                 if (arr2[i] == arr1[j]) {
-                    count+=1;
+                    count++;
                 }
             }
         }
         return count;
     }
-    
 int main() {
-
     int arr1[10] = {1,2,3,4,5,6,7,8,9,10};
     int arr2[3];
     for (int i = 0; i <3; ++i) {
@@ -22,9 +19,6 @@ int main() {
         cin>> n;
         arr2[i] = n;
     }
-
-
-
     if (sub_Arr(arr1, arr2) == 3) {
         cout<<"true";
     }
