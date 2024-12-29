ca28d8933f7077f439b52536868c8aa911757a44
AgastyaDhand1914 <agastyadhand@gmail.com>
2024-12-29 16:06:05 +0530

    merge+quick sort

--- sorting2.cpp
+++ sorting2.cpp
@@ -8,8 +8,15 @@ using namespace std;
 
 //TC: O(nlogn); SC: O(n)
 //requires additional memory for temporary sub-arrays. Therefore, it is not in-place sorting.
+//It is stable.
 
-//APPROACH 1
+//APPLICATION: external sorting, sorting linked lists.
+
+//DRAWBACKS: slower compared to other algos for smaller tasks; extra O(n) memory for temporary arrays;
+//           goes through whole process even if array is sorted
+
+//APPROACH 1: by creating 2 separate arrays and merging
+//APPROACH 2: directly using indices
 
 void merge(int *arr, int s, int e) {
     int dupe = s;
@@ -17,6 +24,8 @@ void merge(int *arr, int s, int e) {
     int len_left = mid - s + 1;
     int len_right = e - mid;
 
+
+    //dividing arr into 2 parts
     int *left = new int[len_left];
     int *right = new int[len_right];
 
@@ -28,6 +37,8 @@ void merge(int *arr, int s, int e) {
         right[i] = arr[l++];
     }
 
+
+    //merging 2 sorted arrays
     int index_left = 0, index_right = 0;
 
     while (index_left<len_left && index_right<len_right) {
@@ -59,6 +70,70 @@ void mergeSort(int *arr, int s, int e) {
     merge(arr, s ,e);
 }
 
+
+
+//QUICK SORT: Recursive Partition
+//We take an element x and place it in the array in such a way that the sub-array on left of x contains 
+//elements which are < x and right sub-array contains elements > x. Thus x works as a partition.
+//Let this position of element xx be p. then we need to repeat the process for sub-arrays :
+//(s, p - 1) & (p + 1, e)
+
+//to get the pivort position of x, we count the number of elements < x. the index of x would does be that
+//number from s (s + count_less_than_x)
+//we then traverse through array and swap remaining elements based upon > x or < x and then call fn again 
+//for resulting sub-arrays.
+
+//TC: O(nlogn), Worst Case: O(n²); SC: O(logn) (avg.)
+//It is in-place sorting.
+//It is 'Tail Recursive'.
+//It is not stable,
+
+//QS is preferred over MS for arrays due to SC and no allocation or deallocation
+//MS is preferred over QS for linked lists as for LL we can insert items in O(1) extra space unlike 
+//for arrays. Moreover arrays have random acces which isused by QS but LL does not: we have to travel 
+//through each and every node. Therefore, there is more overhead for QS. MS accesses data sequentially 
+//which is standard for LL.
+
+
+int partition(int *arr, int s, int e) {
+    //let pivot be the rightmost element
+    int pivot = arr[s];
+    int count_less_than_pivot = 0;
+
+    for (int i = s + 1; i <= e; ++i) {
+        if (arr[i] <= pivot)     count_less_than_pivot++;
+    }
+
+    //correct position of pivot element
+    int pivot_index = s + count_less_than_pivot;
+    swap(arr[s], arr[pivot_index]);
+
+    //checking for remaining elements
+    int i = s, j = e;
+    while (i < pivot_index && j > pivot_index) {
+        while (arr[i] <= pivot)     i++;
+        while (arr[j] > pivot)     j--;
+
+        //we have indices i and j forwhich the elements are incorrectly placed so we simply swap them
+        //not concerned for if the sub-arrays are sorted, it will be handled thrpugh recursion
+        //only concern is clubbing elements > x or < x in their respecive sub-arrays.
+        if (i < pivot_index && j > pivot_index)     swap(arr[i], arr[j]);
+        i++;
+        j--;
+    }
+
+    return pivot_index;
+}
+
+void quickSort(int *arr, int s, int e) {
+    if (s >= e)     return;
+
+    int pos = partition(arr, s, e);
+
+    quickSort(arr, s, pos - 1);
+    quickSort(arr, pos + 1, e);
+}
+
 int main() {
 
     int n;
@@ -69,8 +144,11 @@ int main() {
         cin>>arr[i];
     }
 
-    cout<<"Sorting inputted Array using MERGE sort: "<<endl;
-    mergeSort(arr, 0, n - 1);
+    // cout<<"Sorting inputted Array using MERGE sort: "<<endl;
+    // mergeSort(arr, 0, n - 1);
+
+    cout<<"Sorting inputted Array using QUICK sort: "<<endl;
+    quickSort(arr, 0, n - 1);
 
     for (int i = 0; i<n; ++i) {
         cout<<arr[i]<<" ";
